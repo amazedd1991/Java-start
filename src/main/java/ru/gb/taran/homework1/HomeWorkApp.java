53bdabbdecf746180da371bfa914cee28c899553
Amazedd <amazedd2@mail.ru>
2022-04-16 09:18:05 +0300

    Add checkSumSign

--- src/main/java/ru/gb/taran/homework1/HomeWorkApp.java
+++ src/main/java/ru/gb/taran/homework1/HomeWorkApp.java
@@ -5,13 +5,27 @@ import javax.xml.bind.SchemaOutputResolver;
 public class HomeWorkApp {
     public static void main(String[] args) {
         printThreeWords();
-
+        checkSumSign();
     }
 
     public static void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 5;
+        int b = 10;
+        a = a + b;
+        if(a >= 0 ) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+
+
+
     }
 }
 
