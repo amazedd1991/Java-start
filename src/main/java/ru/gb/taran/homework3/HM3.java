5b2fbed4b27d2e3eb3709e7bc92b86a472c4cff2
Amazedd <amazedd2@mail.ru>
2022-04-20 23:19:24 +0300

    Add method2

--- src/main/java/ru/gb/taran/homework3/HM3.java
+++ src/main/java/ru/gb/taran/homework3/HM3.java
@@ -5,19 +5,28 @@ import java.util.Arrays;
 public class HM3 {
     public static void main(String[] args) {
         method1();
-        }
-        public static void method1() {
-        int[] arr = {0,1,1,0,1};
-            for (int i = 0; i < arr.length; i++) {
-                if(arr[i] <= 0){
-                    arr[i] = 1;
-                } else {
-                    arr[i] = 0;
-                }
-                System.out.println(arr[i]);
+        method2();
+    }
 
+    public static void method1() {
+        int[] arr = {0, 1, 1, 0, 1};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] <= 0) {
+                arr[i] = 1;
+            } else {
+                arr[i] = 0;
             }
+            System.out.println(arr[i]);
 
+        }
+    }
+
+    public static void method2() {
+        int[] a = new int[100];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = i;
+            System.out.println(a[i]);
+        }
     }
 }
 
