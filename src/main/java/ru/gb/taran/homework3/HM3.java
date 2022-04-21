65ca3a31d693b340eedd7cf1bfee9b5fbe3bb2c8
Amazedd <amazedd2@mail.ru>
2022-04-21 08:18:20 +0300

    Add method3

--- src/main/java/ru/gb/taran/homework3/HM3.java
+++ src/main/java/ru/gb/taran/homework3/HM3.java
@@ -1,11 +1,10 @@
 package ru.gb.taran.homework3;
 
-import java.util.Arrays;
-
 public class HM3 {
     public static void main(String[] args) {
         method1();
         method2();
+        method3();
     }
 
     public static void method1() {
@@ -26,6 +25,16 @@ public class HM3 {
         for (int i = 0; i < a.length; i++) {
             a[i] = i;
             System.out.println(a[i]);
+        }
+    }
+
+    public static void method3() {
+        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= 6) {
+                arr[i] *= 2;
+            }
+            System.out.println(arr[i]);
         }
     }
 }
