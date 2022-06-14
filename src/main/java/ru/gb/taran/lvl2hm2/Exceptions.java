4c2e42d09eed8d4121b3522e734cd3dc6e4cb41d
Amazedd <amazedd2@mail.ru>
2022-06-14 21:55:30 +0300

    Add LVL2.HM1

--- src/main/java/ru/gb/taran/lvl2hm2/Exceptions.java
+++ src/main/java/ru/gb/taran/lvl2hm2/Exceptions.java
@@ -1,18 +1,31 @@
 package ru.gb.taran.lvl2hm2;
 
 public class Exceptions {
-    public static void main(String[] args) throws MyArraySizeException, MyArrayDataException {
+    public static void main(String[] args) {
+        try {
+            method();
+        } catch (MyArrayDataException e1) {
+            e1.printStackTrace();
+        } catch (MyArraySizeException e2) {
+            e2.printStackTrace();
+        }
 
+
+    }
+
+
+    public static void method() throws MyArraySizeException, MyArrayDataException {
         String[][] arr = new String[4][4];
         int count = 0;
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i].length > 4) {
+            if (arr[i].length >= 4) {
                 throw new MyArraySizeException();
             }
             for (int j = 0; j < arr[i].length; j++) {
                 try {
                     count = count + Integer.parseInt(arr[i][j]);
                 } catch (NumberFormatException e) {
+                    e.printStackTrace();
                     throw new MyArrayDataException();
                 }
 
